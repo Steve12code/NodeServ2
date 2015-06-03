5ccdb2b136b9a1461f72df1e096d7b4e6a173d00
Steve12code <steve12@stevenjones.us>
2015-06-02 20:21:39 -0500

    About to add users

---
+++ notetest1/public/javascripts/global.js
@@ -0,0 +1,67 @@
+// Userlist data array for filling in info box
+var userListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    populateTable();
+
+
+    // Username link click - you click on linkshowuser class as defined in HTML build in populate table
+    $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/users/userlist', function( data ) {
+
+        // Stick our user data array into a userlist variable in the global object - all the JSON here
+        userListData = data;
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.username + '">' + this.username + '</a></td>';
+            tableContent += '<td>' + this.email + '</td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML table
+        $('#userList table tbody').html(tableContent);
+    });
+};
+
+// Show User Info
+function showUserInfo(event) {
+
+    // Prevent Link from Firing
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute - i.e. you click user1 thisUserName will be user1
+    var thisUserName = $(this).attr('rel');
+
+    // Get Index of object based on id value
+    //      .map means this action occurs on each JSON chunk individually (userListData[0], etc...)
+    //      .indexOf means this action returns a number indicating the location in the array where 'thisUserName' is
+    var arrayPosition = userListData.map(function(arrayItem) { return arrayItem.username; }).indexOf(thisUserName);
+
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoLocation').text(thisUserObject.location);
+
+};
