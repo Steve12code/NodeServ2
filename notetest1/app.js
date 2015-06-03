1845bf150c98944f6381c8e157c3c22d0385d8ed
Steve12code <steve12@stevenjones.us>
2015-06-02 19:23:37 -0500

    updat

--- notetest1/app.js
+++ notetest1/app.js
@@ -1,19 +1,15 @@
-// Various requires listed here again yoah yeah
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// Database
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/notetest1", {native_parser:true});
 
-// New Code to point to my mongoDB 
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/notetest1');
-
-//var store = require('./routes/store');
-var store = require('./routes/store');
-//var users = require('./routes/users');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -29,17 +25,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// New Code to point to my mongoDB
 // Make our db accessible to our router
 app.use(function(req,res,next){
     req.db = db;
     next();
 });
-
-
-app.use('/', store);
-//app.use('/users', users);
-//app.get('/', store.home)
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
